e13e9fd75197ad5e4c0476b389fb7d03ed981437
Edyta Wloszczynska <edyta.wloszczynska@gmail.com>
2022-10-26 12:07:44 +0200

    dist folder

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/mix-it-app/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/mix-it-app/cocktails" : "/",
 };
