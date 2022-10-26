e3364464a77088b5205c911ef778d79a42766030
atyd3 <93736080+atyd3@users.noreply.github.com>
2022-10-26 12:27:31 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/mix-it-up/cocktails/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/mix-it-up/" : "/",
 };
